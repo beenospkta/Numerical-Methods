909e4ca230824418a4cab81a5d53f49f7bf3424c
beenospkta <beenospkta717@gmail.com>
2026-06-10 10:26:42 +0545

    Add files via upload
    TypeCasting in Java

---
+++ TypeCasting.java
@@ -0,0 +1,13 @@
+//TypeCasting in java
+
+public class TypeCasting{
+    public static void main(String[] args){
+        int a = 9;
+        double b = a;
+        System.out.println(b);
+
+        double x = 9.99;
+        int y = (int)x;
+        System.out.println(y);
+    }
+}
